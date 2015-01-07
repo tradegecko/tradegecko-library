36fe94166ea362f12456a9b6599748076d5f3dfc
Wong Pei Yi <wongpeiyi@gmail.com>
2015-01-07 13:20:48 +0800

    Bundler init

---
+++ lib/tradegecko/styles.rb
@@ -0,0 +1,7 @@
+require "tradegecko/styles/version"
+
+module Tradegecko
+  module Styles
+    # Your code goes here...
+  end
+end
---
+++ lib/tradegecko/styles/version.rb
@@ -0,0 +1,5 @@
+module Tradegecko
+  module Styles
+    VERSION = "0.0.1"
+  end
+end
