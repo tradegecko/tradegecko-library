b26b093f3f79ec52f6a6a358bd40f3c7ccbdf308
Wong Pei Yi <wongpeiyi@gmail.com>
2015-02-02 11:43:58 +0800

    grunt and grunt serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,30 @@ module.exports = function(grunt) {
   grunt.initConfig({
     qunit: {
       all: ['tests/*.html']
-    }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 5001,
+          base: ['.', 'tests/'],
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: 'tests/*.js',
+        options: {
+          livereload: true,
+        },
+      },
+    },
   });
 
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['qunit']);
+  grunt.registerTask('serve', ['connect', 'watch']);
 };
